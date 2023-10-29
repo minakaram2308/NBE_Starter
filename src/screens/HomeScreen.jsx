1f9b95d2411c13c7bd8f19af146495405e767a98
Hager44 <hager384@gmail.com>
2023-10-29 13:06:15 +0200

    adding web and pdf

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -7,6 +7,8 @@ import {AccountSummaryNav} from './NavDummyScreens/AccountSummaryNav';
 import {TopBar} from '../components';
 import {DepositsNav} from './NavDummyScreens/DepositsNav';
 import {PaymentsNav} from './NavDummyScreens/PaymentsNav';
+import { Pdf_React } from '../components/PDF-WEB/Pdf_React';
+import { Web_React } from '../components/PDF-WEB/Web_React';
 
 
 const Drawer = createDrawerNavigator();
@@ -22,8 +24,12 @@ export const HomeScreen = ({navigation}) => {
         <Drawer.Screen name="Open Certificates & Deposits" component={AccountSummaryNav} />
         <Drawer.Screen name="Payement Services" component={AccountSummaryNav} />
         <Drawer.Screen name=" Cards Services" component={AccountSummaryNav} />
-        <Drawer.Screen name=" Hard Token" component={AccountSummaryNav} />
-        <Drawer.Screen name="Offers" component={AccountSummaryNav} />
+        <Drawer.Screen name=" Hard Token" component={Web_React} options={{
+            navigation: navigation,
+          }}/>
+        <Drawer.Screen name="Offers" component={Pdf_React} options={{
+            navigation: navigation,
+          }}/>
         <Drawer.Screen name="Customer Services" component={AccountSummaryNav} />
         <Drawer.Screen name="Calculators" component={AccountSummaryNav} />
         <Drawer.Screen name="Dark Mode" component={AccountSummaryNav} />
