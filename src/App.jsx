1f65ebd98ff1847874e073ca6ad9475afefc1e5c
Hadeer1111 <76769568+Hadeer1111@users.noreply.github.com>
2023-10-30 16:13:54 +0200

    main changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React, {useContext, useState} from 'react';
 import {View, StatusBar, Modal} from 'react-native';
 
-import {NavigationContainer} from '@react-navigation/native';
-
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {
   AccountsScreen,
   CardsScreen,
@@ -13,7 +10,7 @@ import {
   HomeScreen,
   LoginScreen,
   SplashScreen,
-  UtilsScreen
+  UtilsScreen,
 } from './screens';
 
 import { SignUpScreen } from './screens/SignUpScreen';
@@ -21,13 +18,15 @@ import { VerificationScreen } from './screens/VerificationScreen'
 import { PasswordScreen } from './screens/PasswordScreen';
 import { CongratulationsScreen } from './screens/CongratulationsScreen';
 
-import {Navigator, TopBar} from './components';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {TopBar, Balance, Navigator} from './components';
 import LoginContext from './store/Authentication/login-context';
-import { colors } from './constants/Colors';
-import styles from "./styles/App.style";
-import { ModeContext, ThemeProvider } from './context/ModeContext';
-import { CashTransferStack } from './components/CashTransferComponents/CashTransferStack';
-import { Maps } from './screens/Maps';
+import {colors} from './constants/Colors';
+import styles from './styles/App.style';
+import {ModeContext, ThemeProvider} from './context/ModeContext';
+import {CashTransferStack} from './components/CashTransferComponents/CashTransferStack';
+import {Maps} from './screens/Maps';
 import AirPayScreen from './screens/AirPayScreen';
 
 const Stack = createNativeStackNavigator();
@@ -40,67 +39,63 @@ export const App = () => {
 
   return (
     <>
-    <ThemeProvider>
-      <Modal
-        visible={loading}
-        animationType="fade"
-        statusBarTranslucent={true}
-        transparent={true}>
-        <SplashScreen onAnimationFinish={setLoading.bind(null, false)} />
-      </Modal>
+      <ThemeProvider>
+        <Modal
+          visible={loading}
+          animationType="fade"
+          statusBarTranslucent={true}
+          transparent={true}>
+          <SplashScreen onAnimationFinish={setLoading.bind(null, false)} />
+        </Modal>
 
-      <NavigationContainer>
-        <View style={styles.app}>
-          {loginContext.loggedIn && (
-            <>
-              <StatusBar
-                barStyle="dark-content"
-                backgroundColor={colors.background}
-                translucent={false}
-              />
-              <Tab.Navigator
-                initialRouteName="home"
-                tabBar={prop => (
-                  <Navigator {...prop} screenOptions={{headerShown: false}} />
-                )}
-                screenOptions={{
-                  header: () => null,
+        <NavigationContainer>
+          <View style={styles.app}>
+            {loginContext.loggedIn && (
+              <>
+                <StatusBar
+                  barStyle="dark-content"
+                  backgroundColor={colors.background}
+                  translucent={false}
+                />
+                <Tab.Navigator
+                  initialRouteName="home"
+                  tabBar={prop => (
+                    <Navigator {...prop} screenOptions={{headerShown: false}} />
+                  )}
+                  screenOptions={{
+                    header: () => null,
+                  }}>
+                  <Tab.Screen name="home" component={HomeScreen} />
+                  <Stack.Screen name="transfer" component={CashTransferStack} />
+                  <Stack.Screen name="cards" component={CardsScreen} />
+                  <Stack.Screen name="map" component={Maps} />
+                  <Stack.Screen name="AirPay" component={AirPayScreen} />
+                </Tab.Navigator>
+              </>
+            )}
 
-               }
-      
-     }
-                >
-                <Tab.Screen name="home" component={HomeScreen} />
-                <Stack.Screen name="transfer" component={CashTransferStack}  />
-                <Stack.Screen name="cards" component={CardsScreen} />
-                <Stack.Screen name="map" component={Maps} />
-                <Stack.Screen name="AirPay" component={AirPayScreen} />
-              </Tab.Navigator>
-            </>
-          )}
+            {!loginContext.loggedIn && (
+              <>
+                <StatusBar
+                  barStyle="dark-content"
+                  backgroundColor={colors.background}
+                  translucent={true}
+                />
 
-          {!loginContext.loggedIn && (
-            <>
-              <StatusBar
-                barStyle="dark-content"
-                backgroundColor={colors.background}
-                translucent={true}
-              />
-
-              <Stack.Navigator
-                initialRouteName="login"
-                screenOptions={{headerShown: false}}>
-                <Stack.Screen name="login" component={LoginScreen} />
-                <Stack.Screen name="signup" component={SignUpScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="verification" component={VerificationScreen} options={{ headerShown: false}} />
-                <Stack.Screen name="password" component={PasswordScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="congratulations" component={CongratulationsScreen} options={{ headerShown: false }} />
-                {/* <Stack.Screen name="login" component={LoginScreen} /> */}
-              </Stack.Navigator>
-            </>
-          )}
-        </View>
-      </NavigationContainer>
+                <Stack.Navigator
+                  initialRouteName="login"
+                  screenOptions={{headerShown: false}}>
+                  <Stack.Screen name="login" component={LoginScreen} />
+                  <Stack.Screen name="signup" component={SignUpScreen} options={{ headerShown: false }} />
+                  <Stack.Screen name="verification" component={VerificationScreen} options={{ headerShown: false}} />
+                  <Stack.Screen name="password" component={PasswordScreen} options={{ headerShown: false }} />
+                  <Stack.Screen name="congratulations" component={CongratulationsScreen} options={{ headerShown: false }} />
+                  {/* <Stack.Screen name="login" component={LoginScreen} /> */}
+                </Stack.Navigator>
+              </>
+            )}
+          </View>
+        </NavigationContainer>
       </ThemeProvider>
     </>
   );
