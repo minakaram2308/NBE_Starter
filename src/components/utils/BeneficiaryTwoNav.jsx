8d0b9ebc55b9b5f02d2e9eeaf30abf1b896577b5
GeorgeKamal <83922448+GeorgeKamal@users.noreply.github.com>
2023-11-02 14:12:25 +0200

    dot

--- src/components/utils/BeneficiaryTwoNav.jsx
+++ src/components/utils/BeneficiaryTwoNav.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {Pressable} from 'react-native';
 import {Text, View, StyleSheet, Image} from 'react-native';
 import {Dimensions} from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 import PressableNavItem from './PressableNavItem';
+import { ModeContext } from '../../Context/ModeContext';
+import { darkColors } from '../../styles/components/Modes/DarkColors';
+import { lightColors } from '../../styles/components/Modes/LightColors';
 function BeneficiaryTwoNav({navigation}) {
+  const {darkTheme, toggle} = useContext(ModeContext);
+  let  styles = darkTheme ? darkColors.darkBackgrd : lightColors.lightBackgrd
+
     function onPressFunction() {
         navigation.navigate("beneficiaries")
     }
 
   return (
-    <View style={style.rootContainer}>
-      <View style={style.navContainer}>
+    <View style={[style.rootContainer, {backgroundColor: styles}]}>
+    <View style={style.navContainer}>
         <View style={{flexDirection: 'row'}}>
           <PressableNavItem
             onPress={onPressFunction}
@@ -39,7 +45,8 @@ export default BeneficiaryTwoNav;
 
 const style = StyleSheet.create({
     rootContainer: {
-      marginTop: 20,
+      // marginTop: 20,
+      paddingTop: 20,
       paddingHorizontal: 20,
       
     },
